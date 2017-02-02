dbdbfa2aa675641305c35aa8a4643f833c627dd8
LambdaXymox <lambda.xymox@gmail.com>
2017-02-01 23:04:45 -0600

    Remove main function.

--- rust/snappy/src/lib.rs
+++ rust/snappy/src/lib.rs
@@ -9,8 +9,3 @@ pub use snappy::uncompress;
 pub use snappy::max_compressed_length;
 pub use snappy::uncompressed_length;
 pub use snappy::validate_compressed_buffer;
-
-fn main() {
-    let x = snappy::max_compressed_length(100);
-    println!("max compressed length of a 100 byte buffer: {}", x);
-}
