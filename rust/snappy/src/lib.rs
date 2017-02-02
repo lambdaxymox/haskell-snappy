d2b349ef51e5b2fbd8bebd6098e2451a343ffc64
LambdaXymox <lambda.xymox@gmail.com>
2017-02-01 23:04:17 -0600

    Expose snappy interface in Rust library.

--- rust/snappy/src/lib.rs
+++ rust/snappy/src/lib.rs
@@ -3,6 +3,13 @@ extern crate libc;
 mod snappy;
 
 
+pub use snappy::SnappyStatus;
+pub use snappy::compress;
+pub use snappy::uncompress;
+pub use snappy::max_compressed_length;
+pub use snappy::uncompressed_length;
+pub use snappy::validate_compressed_buffer;
+
 fn main() {
     let x = snappy::max_compressed_length(100);
     println!("max compressed length of a 100 byte buffer: {}", x);
