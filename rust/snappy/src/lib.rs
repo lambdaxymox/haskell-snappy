3469efa399b2e70440492ea18b66b53f50466dcc
LambdaXymox <lambda.xymox@gmail.com>
2017-02-01 22:47:55 -0600

    Implement snappy API in Rust.

--- rust/snappy/src/lib.rs
+++ rust/snappy/src/lib.rs
@@ -4,8 +4,6 @@ mod snappy;
 
 
 fn main() {
-    let x = unsafe {
-        snappy::snappy_max_compressed_length(100)
-    };
+    let x = snappy::max_compressed_length(100);
     println!("max compressed length of a 100 byte buffer: {}", x);
 }
