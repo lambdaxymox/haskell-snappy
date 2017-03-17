9a0724c776dc67635f24653333086dea4463ecee
LambdaXymox <lambda.xymox@gmail.com>
2017-03-16 22:58:37 -0500

    Add foreign type for Rust byte slices and function for validateCompressedBuffer.

--- Snappy.hs
+++ Snappy.hs
@@ -3,16 +3,29 @@
 module Snappy
     (
         maxCompressedLength,
+        validateCompressedBuffer,
     )
     where 
 
-import Foreign
-import Foreign.C.Types
-import Foreign.ForeignPtr
+import qualified Data.ByteString    as BS
+import           Foreign
+import           Foreign.C.Types
+import           Foreign.ForeignPtr
+
+-- Here we are using the opaque struct pattern to 
+-- represent Rust's slice type.
+data RustSlice
+type RustByteString = ForeignPtr RustSlice
 
 foreign import ccall "max_compressed_length" 
-    c_maxCompressedLength :: CUInt -> CUInt
+    rust_maxCompressedLength :: CUInt -> CUInt
 
+foreign import ccall "validate_compressed_buffer"
+    rust_validateCompressedBuffer :: Ptr RustSlice -> Bool
 
 maxCompressedLength :: Integral a => a -> a
-maxCompressedLength = fromIntegral . c_maxCompressedLength . fromIntegral
+maxCompressedLength = fromIntegral . rust_maxCompressedLength . fromIntegral
+
+validateCompressedBuffer :: RustByteString -> IO Bool
+validateCompressedBuffer fptr = 
+    withForeignPtr fptr (\ptr -> return $ rust_validateCompressedBuffer ptr)
