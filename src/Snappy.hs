d8e6d7dab1b78f523ded5dd5caaa9ddea41be3f0
LambdaXymox <lambda.xymox@gmail.com>
2017-03-16 19:40:01 -0500

    Remove redundant LICENSE save file.

--- src/Snappy.hs
+++ src/Snappy.hs
@@ -13,6 +13,6 @@ import Foreign.ForeignPtr
 foreign import ccall "max_compressed_length" 
     c_maxCompressedLength :: CUInt -> CUInt
 
-
 maxCompressedLength :: Integral a => a -> a
 maxCompressedLength = fromIntegral . c_maxCompressedLength . fromIntegral
+
